2bbdf7b5725b23158b9a7decf1809876e12206aa
mente123 <57287765+mente123@users.noreply.github.com>
2021-06-24 15:29:46 -0700

    Add files via upload

---
+++ behavior.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+  // var thumbnailElement = document.getElementById("smart_thumbnail");
+
+  // thumbnailElement.addEventListener("click", function () {
+  //   if (thumbnailElement.classList.contains("small")) {
+  //     thumbnailElement.className = "";
+  //   } else {
+  //     thumbnailElement.className = "small";
+  //   }
+  // });
+
+  let imageCard = document.querySelectorAll(".g-img");
+
+  imageCard.forEach((el) => {
+    el.addEventListener("click", () => {
+      if (el.classList.contains("active")) {
+        el.classList.remove("active");
+        document.body.style = "overflow: auto;";
+      } else {
+        el.classList.add("active");
+        document.body.style = "overflow: hidden;";
+      }
+    });
+  });
+
+  let contactForm = document.getElementById("contact-form");
+  contactForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    alert(`Sorry Messaging is disabled right now. use my social media to reach me out.`);
+  });
+});
